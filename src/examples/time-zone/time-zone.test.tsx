120db0c12803a7dcd44a434eeca9168aa0c93cc9
wishzx <gabriele.giganti@gmail.com>
2023-07-08 15:35:33 +0200

    exercises

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,22 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers(); // freeze time
+  vi.setSystemTime(new Date(12, 9, 12)); // set to a time
+  //then call setSystemTime again  USE TO move forward animations
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
-test.fails('should match the snapshot', async () => {
-  const { container } = render(<TimeZone />);
+test('should match the snapshot', async () => {
+  const { container } = render(<TimeZone getTodos />);
   expect(container).toMatchSnapshot();
 });
